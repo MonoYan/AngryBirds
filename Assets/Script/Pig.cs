0840efc79f23c2d636549b4a5cc13eea89471249
yanshencc@gmail.com <yanshencc@gmail.com>
2021-01-26 16:45:04 +0800

    Creat enemy; Collision effect

---
+++ Assets/Script/Pig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    public float minSpeed = 5f;
+    private SpriteRenderer render;
+    public Sprite hunted;
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision) //碰撞检测 
+    {
+        if (collision.relativeVelocity.magnitude > maxSpeed)
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.relativeVelocity.magnitude < maxSpeed && collision.relativeVelocity.magnitude > minSpeed)
+        {
+            render.sprite = hunted;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) //触发检测
+    {
+        
+    }
+}
