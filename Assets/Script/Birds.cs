a7df70caa1216a6fbb9d77deec96b263ee886ce6
yanshencc@gmail.com <yanshencc@gmail.com>
2021-01-26 14:49:40 +0800

    MaxDistance

--- Assets/Script/Birds.cs
+++ Assets/Script/Birds.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Birds : MonoBehaviour
 {
     private bool isCLick = false;
+    public Transform rightPos;
+    public float maxDis = 1.25f;
+
     private bool onBirds = false;
     private bool noClick = false;
     public SpringJoint2D springJoint2D;
@@ -27,6 +30,15 @@ public class Birds : MonoBehaviour
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition); //坐标系转换
             transform.position += new Vector3(0,0,1);
+
+            if (Vector3.Distance(transform.position, rightPos.position) > maxDis)  //进行位置限定
+            {
+                Vector3 pos = (transform.position - rightPos.position).normalized;  //单位化向量
+                pos *= maxDis; //最大长度向量
+                transform.position = pos + rightPos.position;
+
+            }
+
             onBirds = true;
         }
 
