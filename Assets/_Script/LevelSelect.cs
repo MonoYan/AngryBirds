574b56be878e0c614dc0c9fa88e10b4527a43e5a
Wang Yan <43719212+MonoYan@users.noreply.github.com>
2021-03-06 16:32:45 +0800

    add levelSelectScript

---
+++ Assets/_Script/LevelSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public bool isSelect = false;
+    public Sprite levelBg;
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    private void Start()
+    {
+        if (transform.parent.GetChild(0).name == gameObject.name)
+        {
+            isSelect = true;
+        }
+        if (isSelect)
+        {
+            image.overrideSprite = levelBg;
+            transform.Find("Num").gameObject.SetActive(true);
+        }
+    }
+}
