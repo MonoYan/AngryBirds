8e5d3e80da2aeaf32a609def3622d6de20613b94
Wang Yan <43719212+MonoYan@users.noreply.github.com>
2021-03-06 15:56:07 +0800

    add mapselect script

---
+++ Assets/_Script/MapSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelect : MonoBehaviour
+{
+    public int startNum = 0;
+    public bool isSelect = false;
+
+    public GameObject locks, starts;
+
+    private void Start()
+    {
+        //存储每一关的星星数量，计算总和
+        
+        if (PlayerPrefs.GetInt("totalNum", 0) >= startNum)
+        {
+            isSelect = true;
+        }
+
+        if (isSelect)
+        {
+            locks.SetActive(false);
+            starts.SetActive(true);
+        }
+    }
+}
